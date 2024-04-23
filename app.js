38a28af77a615bf3d08bf0b018adc19930c5d0b4
KitPolosa <93432249+KitPolosa@users.noreply.github.com>
2024-04-23 23:29:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF'
+tg.MainButton.color = '#2cab37';
+
+let items = [];
+
+function toggleItem(btn, itemId, price) {
+    let item = items.find(i => i.id === itemId);
+    if (!item) {
+        let newItem = { id: itemId, price: price };
+        items.push(newItem);
+        btn.classList.add('added-to-cart');
+        btn.innerText = 'Удалить из корзины';
+        let totalPrice = items.reduce((total, item) => total + item.price, 0);
+        if (totalPrice > 0) {
+            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+            if (!tg.MainButton.isVisible) {
+                tg.MainButton.show();
+            }
+        } else {
+            tg.MainButton.hide();
+        }
+
+    } else {
+        let index = items.indexOf(item);
+        items.splice(index, 1);
+        btn.classList.remove('added-to-cart');
+        btn.innerText = "Добавить в корзину";
+        let totalPrice = items.reduce((total, item) => total + item.price, 0)
+        if (totalPrice > 0) {
+            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+            if (!tg.MainButton.isVisible) {
+                tg.MainButton.show();
+            }
+        } else {
+            tg.MainButton.hide();
+        }
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    let data = {
+        items: items,
+        totalPrice: calculateTotalPrice()
+    };
+    th.SendData(JSON.stringify(data));
+});
+function calculateTotalPrice() {
+    return items.redcue((total, item) => total + item.price, 0);
+}
+
+document.getElementById("btn1").addEventListener("click", function() {
+    toggleItem(this, "item1", 80);
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+    toggleItem(this, "item2", 55);
+});
+
+document.getElementById("btn3").addEventListener("click", function() {
+    toggleItem(this, "item3", 70);
+});
+
+document.getElementById("btn4").addEventListener("click", function() {
+    toggleItem(this, "item4", 47);
+});
+
+document.getElementById("btn5").addEventListener("click", function() {
+    toggleItem(this, "item5", 100);
+});
+
+document.getElementById("btn6").addEventListener("click", function() {
+    toggleItem(this, "item6", 150);
+});
+
+document.getElementById("btn7").addEventListener("click", function() {
+    toggleItem(this, "item7", 110);
+});
+
+document.getElementById("btn8").addEventListener("click", function() {
+    toggleItem(this, "item8", 70);
+});
---
+++ main.py
@@ -0,0 +1,65 @@
+from aiogram import Bot, Dispatcher, F
+import asyncio
+from dotenv import load_dotenv
+import os
+import json
+from utilits.commands import set_commands
+from handlers.start import get_start
+from state.register import RegisterState
+from handlers.register import start_register, register_name, register_phone, register_address
+from aiogram.filters import Command
+
+load_dotenv()
+
+token = os.getenv('TOKEN')
+admin_id = os.getenv('ADMIN_ID')
+
+bot = Bot(token=token, parse_mode='HTML')
+dp = Dispatcher()
+
+async def start_bot(bot: Bot):
+    await bot.send_message(admin_id, text='Здарова заебал')
+
+dp.startup.register(start_bot)
+dp.message.register(get_start, Command(commands='start'))
+
+@dp.message()
+async def web_app(callback_query):
+    json_data = callback_query.web_app_data.data
+    parsed_data = json.loads(json_data)
+    message = ""
+    for i, item in enumerate(parsed_data['items'], start=1):
+        position = int(item['id'].replace('item', ''))
+        message += f"Позиция {position}\n"
+        message += f"Стоимость: {item['price']}\n\n"
+
+    message += f"Общая стоимость: {parsed_data['totalPrice']}"
+
+    await bot.send_message(callback_query.from_user.id, f"""
+{message}
+""")
+
+    await bot.send_message('-1002063166054', f"""
+Новый заказ ✅
+
+{message}
+""")
+
+
+#Регистрируем хендлеры регистрации
+dp.message.register(start_register, F.text=='Зарегистрироваться')
+dp.message.register(register_name, RegisterState.regName)
+dp.message.register(register_phone, RegisterState.regPhone)
+dp.message.register(register_address, RegisterState.regAddress)
+
+
+
+async def start():
+    await set_commands(bot)
+    try:
+        await dp.start_polling(bot, skip_updates=True)
+    finally:
+        await bot.session.close()
+
+if __name__ == '__main__':
+    asyncio.run(start())
