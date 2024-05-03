69559eba104c49bd26240e1fedf65fcfdaeccb0b
KitPolosa <93432249+KitPolosa@users.noreply.github.com>
2024-05-03 21:26:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 let items = {
-    item1: { id: "item1", price: 80, quantity: 0 },
-    item2: { id: "item2", price: 120, quantity: 0 },
-    item3: { id: "item3", price: 70, quantity: 0 },
-    item4: { id: "item4", price: 470, quantity: 0 },
-    item5: { id: "item5", price: 100, quantity: 0 },
-    item6: { id: "item6", price: 150, quantity: 0 },
-    item7: { id: "item7", price: 110, quantity: 0 },
-    item8: { id: "item8", price: 70, quantity: 0 }
+    item1: { id: "item1", price: 80, quantity: 0, name: "Молоко коровка из кореновки 2,5%" },
+    item2: { id: "item2", price: 120, quantity: 0, name: "Масло сливочное кубанский молочник 82,5%" },
+    item3: { id: "item3", price: 70, quantity: 0, name: "Творог коровка из кореновки 9%" },
+    item4: { id: "item4", price: 470, quantity: 0, name: "Пломбир коровка из кореновки" },
+    item5: { id: "item5", price: 100, quantity: 0, name: "Кефир коровка из кореновки 2,5%" },
+    item6: { id: "item6", price: 150, quantity: 0, name: "Ряженка коровка из кореновки 2,5%" },
+    item7: { id: "item7", price: 110, quantity: 0, name: "Йогурт коровка из кореновки" },
+    item8: { id: "item8", price: 70, quantity: 0, name: "Сгущенка коровка из кореновки 8,5%" }
 };
 
 function updateQuantity(itemId, change) {
